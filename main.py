1e438125eb9ba02882d16ac765641dcce3bf6f92
Dotun <dotunjolaosho@gmail.com>
2020-04-21 12:00:27 +0100

    fixes

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import os
 import slack
-import redis
+import re
 from dotenv import load_dotenv
 from flask import Flask, request, Response
+import requests
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 load_dotenv()
 app = Flask(__name__)
 
-
-redisClient = redis.Redis(decode_responses=True)
-slackClient = slack.WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
-account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-twilioClient = Client(account_sid, auth_token)
-
+slack_token=os.getenv("SLACK_BOT_TOKEN")
+slackClient = slack.WebClient(slack_token)
+twilioClient = Client()
 
 @app.route('/incoming/twilio', methods=['POST'])
 def send_incoming_message():
@@ -23,30 +20,43 @@ def send_incoming_message():
     message = f"Text message from {from_number}: {sms_message}"
     slack_message = slackClient.chat_postMessage(
         channel='#general', text=message, icon_emoji=':robot_face:')
-    message_id = slack_message['ts']
-    attributes = {message_id: from_number}
-    redis_message = redisClient.mset(attributes)
     response = MessagingResponse()
     return Response(response.to_xml(), mimetype="text/html"), 200
 
 @app.route('/incoming/slack', methods=['POST'])
 def send_incoming_slack():
     attributes = request.get_json()
-    incoming_slack_message_id, slack_message = parse_message(attributes)
+    if 'challenge' in attributes:
+      return Response(attributes['challenge'], mimetype="text/plain")
+    incoming_slack_message_id, slack_message, channel = parse_message(attributes)
     if incoming_slack_message_id and slack_message:
-        to_number = redisClient.get(incoming_slack_message_id)
+        to_number = get_to_number(incoming_slack_message_id, channel)
         if to_number:
             messages = twilioClient.messages.create(
                 to=to_number, from_=os.getenv("TWILIO_FROM"), body=slack_message)
         return Response(), 200
     return Response(), 200
 
-
 def parse_message(attributes):
     if 'event' in attributes and 'thread_ts' in attributes['event']:
-        return attributes['event']['thread_ts'], attributes['event']['text']
-    return None, None
+        return attributes['event']['thread_ts'], attributes['event']['text'], attributes['event']['channel']
+    return None, None, None
 
+def get_to_number(incoming_slack_message_id, channel):
+    r = requests.get(f"https://slack.com/api/conversations.history?token={slack_token}&channel={channel}&latest={incoming_slack_message_id}&limit=1&inclusive=true")
+    if r.status_code == 200:
+       data = r.json()
+       if 'messages' in data and 'subtype' in data['messages'][0] and data['messages'][0]['subtype'] == 'bot_message': 
+           phone_number = extract_phone_number(text)
+           return phone_number
+    return None
+
+def extract_phone_number(text): 
+    data = re.findall(r'\w+', text)
+    if len(data) >= 4: 
+      return data[3]
+    return None
 
 if __name__ == '__main__':
     app.run()
+
