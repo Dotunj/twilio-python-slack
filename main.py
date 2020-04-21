3cb90484018c907239fe1a58a22d84d79b6820eb
Dotun <dotunjolaosho@gmail.com>
2020-04-21 20:52:45 +0100

    removed request library

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import slack
 import re
 from dotenv import load_dotenv
 from flask import Flask, request, Response
-import requests
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 load_dotenv()
 app = Flask(__name__)
 
 slack_token=os.getenv("SLACK_BOT_TOKEN")
-slackClient = slack.WebClient(slack_token)
-twilioClient = Client()
+slack_client = slack.WebClient(slack_token)
+twilio_client = Client()
+
 
 @app.route('/incoming/twilio', methods=['POST'])
 def send_incoming_message():
     from_number = request.form['From']
     sms_message = request.form['Body']
     message = f"Text message from {from_number}: {sms_message}"
-    slack_message = slackClient.chat_postMessage(
+    slack_message = slack_client.chat_postMessage(
         channel='#general', text=message, icon_emoji=':robot_face:')
     response = MessagingResponse()
     return Response(response.to_xml(), mimetype="text/html"), 200
@@ -32,7 +32,7 @@ def send_incoming_slack():
     if incoming_slack_message_id and slack_message:
         to_number = get_to_number(incoming_slack_message_id, channel)
         if to_number:
-            messages = twilioClient.messages.create(
+            messages = twilio_client.messages.create(
                 to=to_number, from_=os.getenv("TWILIO_NUMBER"), body=slack_message)
         return Response(), 200
     return Response(), 200
@@ -43,13 +43,11 @@ def parse_message(attributes):
     return None, None, None
 
 def get_to_number(incoming_slack_message_id, channel):
-    r = requests.get(f"https://slack.com/api/conversations.history?token={slack_token}&channel={channel}&latest={incoming_slack_message_id}&limit=1&inclusive=true")
-    if r.status_code == 200:
-       data = r.json()
-       if 'messages' in data and 'subtype' in data['messages'][0] and data['messages'][0]['subtype'] == 'bot_message': 
-           text = data['messages'][0]['text']
-           phone_number = extract_phone_number(text)
-           return phone_number
+    data = slack_client.conversations_history(channel=channel, latest=incoming_slack_message_id, limit=1, inclusive=1)
+    if 'subtype' in data['messages'][0] and data['messages'][0]['subtype'] == 'bot_message': 
+        text = data['messages'][0]['text']
+        phone_number = extract_phone_number(text)
+        return phone_number
     return None
 
 def extract_phone_number(text): 
